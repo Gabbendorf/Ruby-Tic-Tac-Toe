103baa759b6f41a19683a198c8cae9b1e196b79b
Gabriella <gabriellamds@yahoo.it>
2017-07-20 12:32:35 +0100

    places mark

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,6 +1,6 @@
 class Grid
 
-attr_reader :size
+attr_reader :size, :cells
 
   def initialize(size)
     @size = size
@@ -11,10 +11,23 @@ attr_reader :size
     @cells = (0..grid_dimension-1).map {|cell| cell = nil}
   end
 
+  def place_mark(grid_position, mark)
+    cell_number = grid_position.to_i - 1
+    if cell_empty?(cell_number)
+      @cells[cell_number] = mark
+    else
+      :already_occupied
+    end
+  end
+
   private
 
   def grid_dimension
     @size * @size
+  end
+
+  def cell_empty?(cell_number)
+    @cells[cell_number] == nil
   end
 
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -17,4 +17,26 @@ RSpec.describe Grid do
     expect(grid_cells).to eq([nil, nil, nil, nil, nil, nil, nil, nil, nil])
   end
 
+  it "places mark on grid if it's empty" do
+    mark = "X"
+    grid_position = "3"
+
+    grid.place_mark(grid_position, mark)
+
+    grid_cell = grid.cells[2]
+    expect(grid_cell).to eq("X")
+  end
+
+  it "returns :already_occupied if cell is already occupied" do
+    mark = "X"
+    grid_position = "3"
+    grid.place_mark(grid_position, mark)
+
+    mark2 = "O"
+    grid_position = "3"
+    result = grid.place_mark(grid_position, mark2)
+
+    expect(result).to eq(:already_occupied)
+  end
+
 end
