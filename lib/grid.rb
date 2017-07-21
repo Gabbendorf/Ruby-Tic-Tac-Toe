994ee4798f33d484f84b46da267edc9301cc8f0b
Gabriella <gabriellamds@yahoo.it>
2017-07-21 15:29:34 +0100

    implements rules for ending game

--- lib/grid.rb
+++ lib/grid.rb
@@ -22,11 +22,19 @@ attr_reader :size, :cells
 
   def prepare_grid
     grid_numbers = (1..grid_dimension).map {|number| number}
-    cells = @cells.map.with_index { |cell, index| cell.nil? ? grid_numbers[index] : cell }
-    cells.each_slice(@size).to_a
+    filled_cells = @cells.map.with_index { |cell, index| cell.nil? ? grid_numbers[index] : cell }
+    filled_cells.each_slice(@size).to_a
+  end
+
+  def end_game?
+    won? || draw?
   end
 
   private
+
+  def rows
+    @cells.map.each_slice(@size).to_a
+  end
 
   def grid_dimension
     @size * @size
@@ -36,4 +44,58 @@ attr_reader :size, :cells
     @cells[cell_number] == nil
   end
 
+  def first_diagonal_row
+    first_diagonal_row = []
+    index = 0
+    rows.each do |row|
+      first_diagonal_row.push(row[index])
+      index += 1
+    end
+    first_diagonal_row
+  end
+
+  def second_diagonal_row
+    second_diagonal_row = []
+    index = @size-1
+    rows.each do |row|
+      second_diagonal_row.push(row[index])
+      index -= 1
+    end
+    second_diagonal_row
+  end
+
+  def won?
+    horizontal_winning_row? || vertical_winning_row? || diagonal_winning_row?
+  end
+
+  def draw?
+    full? && no_winning_rows?
+  end
+
+  def full?
+    @cells.all? {|cell| cell != nil}
+  end
+
+  def no_winning_rows?
+    !horizontal_winning_row? || !vertical_winning_row? || !diagonal_winning_row?
+  end
+
+  def horizontal_winning_row?
+    winning_rows = rows.select {|row| row.uniq.size == 1 &&
+      row.uniq.first != nil}
+    !winning_rows.empty?
+  end
+
+  def vertical_winning_row?
+    winning_rows = rows.transpose.select {|row| row.uniq.size == 1 &&
+      row.uniq.first != nil}
+    !winning_rows.empty?
+  end
+
+  def diagonal_winning_row?
+    diagonal_rows = [first_diagonal_row, second_diagonal_row]
+    winning_rows = diagonal_rows.select {|row| row.uniq.size == 1 &&
+      row.uniq.first != nil}
+    !winning_rows.empty?
+  end
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -18,34 +18,34 @@ RSpec.describe Grid do
   end
 
   it "places mark on grid if it's empty" do
-    mark = "X"
+    mark = :X
     grid_position = "3"
 
     grid.place_mark(grid_position, mark)
 
     grid_cell = grid.cells[2]
-    expect(grid_cell).to eq("X")
+    expect(grid_cell).to eq(:X)
   end
 
   it "places second mark on grid if it's empty" do
-    mark = "X"
+    mark = :X
     grid_position = "3"
     grid.place_mark(grid_position, mark)
 
-    mark2 = "O"
+    mark2 = :O
     grid_position = "6"
     grid.place_mark(grid_position, mark2)
 
-    grid_cell = grid.cells[5]
-    expect(grid_cell).to eq("O")
+    grid_cells = grid.cells
+    expect(grid_cells).to eq([nil, nil, :X, nil, nil, :O, nil, nil, nil])
   end
 
   it "returns :already_occupied if cell is already occupied by mark" do
-    mark = "X"
+    mark = :X
     grid_position = "3"
     grid.place_mark(grid_position, mark)
 
-    mark2 = "O"
+    mark2 = :O
     grid_position = "3"
     result = grid.place_mark(grid_position, mark2)
 
@@ -58,14 +58,44 @@ RSpec.describe Grid do
     expect(cells_display).to eq([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
   end
 
-  it "prepares array of arrays of numbers for correspondent empty cells and of placed marks" do
-    mark = "X"
+  it "prepares array of arrays of numbers for correspondent empty cells and of placed marks if there are any" do
+    mark = :X
     grid_position = "3"
     grid.place_mark(grid_position, mark)
 
     cells_display = grid.prepare_grid
 
-    expect(cells_display).to eq([[1, 2, "X"], [4, 5, 6], [7, 8, 9]])
+    expect(cells_display).to eq([[1, 2, :X], [4, 5, 6], [7, 8, 9]])
+  end
+
+  it "returns true if game ends because someone wins" do
+    grid.place_mark("3", :X)
+    grid.place_mark("5", :X)
+    grid.place_mark("7", :X)
+
+    expect(grid.end_game?).to eq(true)
+  end
+
+  it "returns true if game ends and it's draw" do
+    grid.place_mark("1", :X)
+    grid.place_mark("2", :O)
+    grid.place_mark("3", :X)
+    grid.place_mark("4", :O)
+    grid.place_mark("5", :X)
+    grid.place_mark("6", :O)
+    grid.place_mark("8", :X)
+    grid.place_mark("7", :O)
+    grid.place_mark("9", :X)
+
+    expect(grid.end_game?).to eq(true)
+  end
+
+  it "returns false if game is not finished" do
+    grid.place_mark("1", :X)
+    grid.place_mark("2", :O)
+    grid.place_mark("3", :X)
+
+    expect(grid.end_game?).to eq(false)
   end
 
 end
